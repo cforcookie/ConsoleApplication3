1bbab93f829b72a01fabd6ae72b6cfc279507c50
flotry <jastbast1103@outlook.com>
2022-03-21 14:01:33 +0300

    Код закоментирован

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -2,21 +2,25 @@
 
 int main()
 {   
-    int number;
-    int len_number = 0;
+    int number; //Переменная для хранения длинны массиыв
+    int len_number = 0; //Переменная для хранения числа четных чисел
+    //Ввод в переменную number
     std::cout << "Write length array: ";
     std::cin >> number;
-    int *A = new int[number];
+    int *A = new int[number]; //Динамический массив
+    //Заполнение динамического массива
     for (int i = 0; i < number; i++)
     {
         A[i] = i;
+        //Подсчет колличества четных чисел
         if (i % 2 == 0)
         {
             len_number++;
         }
     }
-    int *B = new int[len_number];
-    int j = 0;
+    int *B = new int[len_number]; //Динамический массив для четных чисел
+    int j = 0; //Счетчик индекса массива B
+    //Заполнение массива B
     for (int i = 0; i < number; i++)
     {
         if (A[i] % 2 == 0)
@@ -25,6 +29,7 @@ int main()
             j++;
         }
     }
+    //Модуль вывода значений массива в виде матрицы
     std::cout << "Result:" << std::endl;
     int x = 0;
     for (int i = 0; i < len_number; i++)
